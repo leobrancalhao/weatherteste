3a38b1b2c8bb269f114bcbb702c51c1aede6c123
leobrancalhao <leobrancalhao@gmail.com>
2019-03-10 12:39:41 -0300

    Interacao com a tela de detalhes

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,12 +1,28 @@
-import React,{Component} from "react"
+import React, { Component } from "react"
 
-import {View, Text} from "react-native" 
+import { View, Text } from "react-native"
+export default class Detail extends Component
+{
 
-export default class Detail extends Component{
-    render() {
-        return ( 
+    static navigationOptions = {
+        title: 'Detail day'
+    };
+
+    constructor( props )
+    {
+        super( props )
+        this.state = {
+            previsaoDetalhadaDia: this.props.navigation.state.params.previsaoDetalhadaDia
+        };
+
+    }
+
+    render ()
+    {
+        return (
             <View>
                 <Text>Segunda tela</Text>
+                <Text>Detalhes {this.state.previsaoDetalhadaDia.main.temp}</Text>
             </View>
         );
     }
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, FlatList, TouchableWithoutFeedback } from 'react-native';
 import api from "../services/api";
 import Icons from "../images/icons"
 
@@ -16,7 +16,6 @@ export default class Main extends Component
 
 
     state = {
-        weather: [],
         cidade: "",
         pais: "BR",
         tempo: "",
@@ -60,7 +59,7 @@ export default class Main extends Component
         try 
         {
             city = this.state.cidadeEncontrada + "," + this.state.pais;
-            const responseCurrentWeather = await api.get( `/find?q=${ city }"&lang=pt&units=metric&APPID=${ APPID }` )
+            const responseCurrentWeather = await api.get( `/forecast?q=${ city }"&lang=pt&units=metric&APPID=${ APPID }` )
 
             console.log( responseCurrentWeather.data.list )
             this.setState( {
@@ -73,7 +72,21 @@ export default class Main extends Component
         }
     };
 
-    render () 
+    space ()
+    {
+        return ( <View style={{ height: 50, width: 1, backgroundColor: 'gray', margin: 5 }} /> )
+    }
+
+    actionOnRow ( item )
+    {
+
+
+        console.log( 'Selected Item :', item );
+
+        this.props.navigation.navigate( "Detail", { previsaoDetalhadaDia: item } )
+    }
+
+    render ()
     {
         return (
             <View style={styles.container}>
@@ -95,7 +108,21 @@ export default class Main extends Component
                             returnKeyType={"search"}
                             onSubmitEditing={this.getWeather} />
 
-                        <Button title="Atualizar" onPress={() => this.props.navigation.navigate( "Detail" )} style={{ borderRadius: 10, margin: 5, }}></Button>
+                        {/* <Button title="Atualizar" onPress={() => this.props.navigation.navigate( "Detail" )} style={{ borderRadius: 10, margin: 5, }}></Button> */}
+
+                    </View>
+                    <View>
+                        <FlatList
+                            data={this.state.dias}
+                            keyExtractor={( item, index ) => 'key' + index}
+                            ItemSeparatorComponent={this.space}
+                            horizontal={true}
+                            renderItem={( { item } ) =>
+                                <TouchableWithoutFeedback onPress={() => this.actionOnRow( item )}>
+                                    <Text style={{ fontSize: 20, textAlign: 'center', }}>{item.weather[ 0 ].description}</Text>
+                                </TouchableWithoutFeedback>
+                            }
+                        />
                     </View>
                 </View>
             </View >
