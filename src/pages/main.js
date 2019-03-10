db864c796f194d03f3918b7c590bdb659816e563
leobrancalhao <leobrancalhao@gmail.com>
2019-03-10 01:44:59 -0300

    Recebendo valores e atualizando a tela

--- src/pages/main.js
+++ src/pages/main.js
@@ -15,12 +15,12 @@ export default class Main extends Component
 
 
 
-    state = { 
+    state = {
         weather: [],
         cidade: "",
         pais: "BR",
-        tempo: "Clear",
-        temperatura: 21,
+        tempo: "",
+        temperatura: 0,
         cidadeEncontrada: "Blumenau",
         icone: Icons(),
         dias: []
@@ -28,7 +28,6 @@ export default class Main extends Component
     componentDidMount ()
     {
         this.getWeather();
-        this.getForecast();
     }
 
 
@@ -37,16 +36,18 @@ export default class Main extends Component
         try
         {
             city = this.state.cidadeEncontrada + "," + this.state.pais;
-            const responseCurrentWeather = await api.get( `/weather?q=${ city }"&units=metric&APPID=${ APPID }` )
+            const responseCurrentWeather = await api.get( `/weather?q=${ city }"&lang=pt&units=metric&APPID=${ APPID }` )
 
             //console.log( responseCurrentWeather.data.name )
             this.setState( {
-                cidade: responseCurrentWeather.data.name,
+                cidade: responseCurrentWeather.data.name + ',' + responseCurrentWeather.data.sys.country,
                 pais: responseCurrentWeather.data.sys.country,
-                tempo: responseCurrentWeather.data.weather[ 0 ].main,
+                tempo: responseCurrentWeather.data.weather[ 0 ].description,
                 temperatura: responseCurrentWeather.data.main.temp,
                 icone: Icons( responseCurrentWeather.data.weather[ 0 ].icon )
             } );
+
+            await this.getForecast();
 
         } catch ( error ) 
         {
@@ -59,7 +60,7 @@ export default class Main extends Component
         try 
         {
             city = this.state.cidadeEncontrada + "," + this.state.pais;
-            const responseCurrentWeather = await api.get( `/find?q=${ city }"&units=metric&APPID=${ APPID }` )
+            const responseCurrentWeather = await api.get( `/find?q=${ city }"&lang=pt&units=metric&APPID=${ APPID }` )
 
             console.log( responseCurrentWeather.data.list )
             this.setState( {
@@ -76,13 +77,13 @@ export default class Main extends Component
     {
         return (
             <View style={styles.container}>
-                <Text style={{ textAlign: 'center', fontSize: 20, fontStyle: "italic" }}>{this.state.cidade}</Text>
+                <Text style={{ textAlign: 'center', fontSize: 40, fontStyle: "italic" }}>{this.state.cidade}</Text>
                 <Text style={styles.icon}>
                     {this.state.icone}
                 </Text>
                 <View >
-                    {/* <Text>{this.state.main.pressure || ""}</Text>
-                    <Text>{this.state.main.temp || ""}</Text> */}
+                    <Text style={{ textAlign: 'center', fontSize: 60, fontStyle: "italic" }}>{Math.round( this.state.temperatura ) + "°C" || ""}</Text>
+                    <Text style={{ textAlign: 'center', fontSize: 20, fontStyle: "italic" }}>{this.state.tempo || ""}</Text>
                     <View>
                         <TextInput style={{ height: 40, borderColor: 'black', borderWidth: 1, borderRadius: 5, margin: 5 }}
                             placeholder="Digite a cidade"
@@ -94,7 +95,7 @@ export default class Main extends Component
                             returnKeyType={"search"}
                             onSubmitEditing={this.getWeather} />
 
-                        <Button title="Load Weather" onPress={() => this.props.navigation.navigate( "CitiesLocalList" )} style={{ borderRadius: 10, margin: 5, }}></Button>
+                        <Button title="Atualizar" onPress={() => this.props.navigation.navigate( "Detail" )} style={{ borderRadius: 10, margin: 5, }}></Button>
                     </View>
                 </View>
             </View >
@@ -110,7 +111,7 @@ const styles = StyleSheet.create( {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'stretch',
-        margin: 10
+        margin: 5
     },
     icon: {
         textAlign: 'center',
